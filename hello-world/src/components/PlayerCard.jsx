719beee3668770973d17c1ba75af4ded64f9948a
Luis Andres Contreras <32722767+tremenditomen@users.noreply.github.com>
2023-02-09 10:39:29 -0700

    just added some margins and spacing for the player card

--- hello-world/src/components/PlayerCard.jsx
+++ hello-world/src/components/PlayerCard.jsx
@@ -20,15 +20,15 @@ const PlayerCard = () => {
       <>
 
   
-<div className='flex justify-center flex-col border-2 items-center border-black' >
+<div className='flex justify-center flex-col border-2 items-center border-black w-full' >
 
 
     <h1 className = "text-3xl font-bold underline">testing out apis with ajax </h1>
-    <input  className="block p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type={'text'} placeholder={"enter LOL name here"} key="gamertag" onChange={(e)=>setCurrentInput(e.target.value)} value={currentInput} ></input>
-    <button className="text-white  right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={()=>handleClick(currentInput,setCurrentInput,setRank,setTier,setRankPoints,setWins,setLost,setPlayerData,setRankedId)} > search</button>
+    <input  className="block p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mt-5" type={'text'} placeholder={"enter LOL name here"} key="gamertag" onChange={(e)=>setCurrentInput(e.target.value)} value={currentInput} ></input>
+    <button className="text-white  right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-5" onClick={()=>handleClick(currentInput,setCurrentInput,setRank,setTier,setRankPoints,setWins,setLost,setPlayerData,setRankedId)} > search</button>
 
 
-<div className= 'flex  flex-col border-2 items-center border-black rounded-lg  dark:bg-gray-700 dark:text-white focus:ring-blue-500'>
+<div className= 'flex  flex-col border-2 items-center border-black rounded-lg  dark:bg-gray-700 dark:text-white focus:ring-blue-500 mt-5 w-2/5 p-5'>
 
 
 {JSON.stringify(playerData) !== "{}" ?<> <p>{playerData.name}</p>
